b054126d3a4b50e8b8ac85f1448b2b7ef87e1baf
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-01-14 12:38:13 +0600

    shadcn some component added

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import TodoCard from './TodoCard';
+import { Button } from '../ui/button';
+import AddTodoModal from './AddTodoModal';
 
 const TodoContainer = () => {
     return (
         <div>
-            <div>
-                <button>Add todo</button>
+            <div className='flex justify-between mb-5'>
+
+                <AddTodoModal></AddTodoModal>
                 <button>Filter</button>
             </div>
-            <div className='bg-green-500 w-full h-full rounded-xl p-5 space-y-5'>
+            <div className='bg-primary-gradient w-full h-full rounded-xl p-[5px]'>
+
+                <div className='bg-white p-5 space-y-5 w-full rounded-lg'>
+                    <TodoCard></TodoCard>
+                    <TodoCard></TodoCard>
+                    <TodoCard></TodoCard>
+                </div>
+
                 {/* <div className='bg-white font-bold p-4 text-2xl rounded-md flex justify-center items-center'>
                     <p>
                         There is no Task Pending
                     </p>                </div> */}
+
+
             </div>
         </div>
     );
