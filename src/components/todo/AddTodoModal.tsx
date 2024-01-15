5ade89979cb7943eb419edf542e06e2f7ca4c6a5
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-01-15 10:07:59 +0600

    add todo and toggle todo

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -3,15 +3,28 @@ import { Button } from "../ui/button";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { addTodo } from "@/redux/features/todoSlice";
 
 const AddTodoModal = () => {
     const [task, setTask] = useState('');
     const [description, setDescription] = useState('');
+    const dispatch = useAppDispatch();
+
+    const { todos } = useAppSelector(state => state.todos)
 
     const onSubmit = (e: FormEvent) => {
         e.preventDefault();
 
-        console.log({ task, description })
+        const randomString = Math.random().toString(36).substring(2, 7);
+
+        const taskDetails = {
+            id: randomString,
+            title: task,
+            description: description,
+        }
+
+        dispatch(addTodo(taskDetails))
     }
 
     return (
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,8 +1,12 @@
 import TodoCard from './TodoCard';
 import AddTodoModal from './AddTodoModal';
 import TodoFilter from './TodoFilter';
+import { useAppSelector } from '@/redux/hooks';
 
 const TodoContainer = () => {
+    const { todos } = useAppSelector(state => state.todos)
+
+
     return (
         <div>
             <div className='flex justify-between mb-5'>
@@ -15,9 +19,11 @@ const TodoContainer = () => {
             <div className='bg-primary-gradient w-full h-full rounded-xl p-[5px]'>
 
                 <div className='bg-white p-5 space-y-5 w-full rounded-lg'>
-                    <TodoCard></TodoCard>
-                    <TodoCard></TodoCard>
-                    <TodoCard></TodoCard>
+                    {
+                        todos.map((item) => (
+                            <TodoCard {...item}></TodoCard>
+                        ))
+                    }
                 </div>
 
                 {/* <div className='bg-white font-bold p-4 text-2xl rounded-md flex justify-center items-center'>
@@ -27,7 +33,7 @@ const TodoContainer = () => {
 
 
             </div>
-        </div>
+        </div >
     );
 };
 
